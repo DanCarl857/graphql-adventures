5f4ab7000810431b9c6a38f5c8ab1faaef6df774
Daniel Carlson <dancarl758@gmail.com>
2019-02-05 21:45:16 +0100

    Worked on Schemas and resolvers

--- event-booking/app.js
+++ event-booking/app.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema,  } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello world');
-})
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
 
-app.listen(3000);
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+           return ['Romantic Cooking', 'Sailing', 'All night coding'] 
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}));
+
+app.listen(3007);
