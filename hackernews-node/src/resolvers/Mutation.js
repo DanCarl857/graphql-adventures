463ee53032662633817e73c3bc7a49ac6fb45a0e
Daniel Carlson <dancarl758@gmail.com>
2019-02-18 23:32:32 +0100

    graphql realtime subscriptions

--- hackernews-node/src/resolvers/Mutation.js
+++ hackernews-node/src/resolvers/Mutation.js
@@ -32,6 +32,22 @@ async function login(parent, args, context, info) {
     }
 }
 
+async function vote(parent, args, context, info) {
+    const userId = getUserId(context)
+    const linkExists = await context.prisma.$exists.vote({
+        user: { id: userId },
+        link: { id: args.linkId },
+    })
+    if (linkExists) {
+        throw new Error(`Already voted for links: ${args.linkId}`)
+    }
+
+    return context.prisma.createVote({
+        user: { connect: { id: userId } },
+        link: { connect: { id: args.linkId } },
+    })
+}
+
 function post(parent, args, context, info) {
     const userId = getUserId(context)
     return context.prisma.createLink({
@@ -45,4 +61,5 @@ module.exports = {
     signup,
     login,
     post,
+    vote,
 }
