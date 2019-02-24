480486e6ff392a0991dfc9bffda78cfd2f179332
Daniel Carlson <dancarl758@gmail.com>
2019-02-24 23:08:23 +0100

    Working on Types in Graphql

--- server-boilerplate/src/index.js
+++ server-boilerplate/src/index.js
@@ -1,24 +1,48 @@
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+
 const schema = gql`
     type Query {
         me: User
+        user(id: ID!): User
+        users: [User!]
     }
 
     type User {
+        id: ID!
         username: String!
     }
 `;
 
+let users = {
+    1: {
+      id: '1',
+      username: 'Robin Wieruch',
+    },
+    2: {
+      id: '2',
+      username: 'Dave Davids',
+    },
+  };
+
+const me = users[1];
+
 const resolvers = {
     Query: {
         me: () => {
-            return {
-                username: 'Daniel Carlson',
-            };
+            return me;
         },
+        user: (parent, { id }) => {
+            return users[id];
+        },
+        users: () => {
+            return Object.values(users);
+        }
     },
 };
 
