186e049eb522654d62ea34bf54f9c90ce88fc5f9
Daniel Carlson <dancarl758@gmail.com>
2019-02-26 14:18:28 +0100

    Added some validation and errors
    * Connected database to resolvers and update user model with custom method
    
    * Added some validation and errors

--- server-boilerplate/src/resolvers/user.js
+++ server-boilerplate/src/resolvers/user.js
@@ -7,6 +7,9 @@ export default {
             return await models.User.findById(id);
         },
         me: async (parent, args, { me }) => {
+            if (!me ) {
+                return null;
+            }
             return await models.User.findById(me.id);
         },
     },
